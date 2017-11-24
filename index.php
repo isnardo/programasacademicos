52bf826d0652839a5b518c7b3c949e5834cd4571
isnardo <isnardo.rr@gmail.com>
2017-11-24 09:15:09 -0600

    Prueba conexion

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  * An open source application development framework for PHP
  *
- * This content is released under the MIT License (MIT)
+ * This content is released under the MI License (MIT)
  *
  * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
  *
